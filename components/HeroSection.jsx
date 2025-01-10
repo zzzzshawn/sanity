45ff6b9a063a95a17dc97039ad1f81da424eb5cc
Kuzuri247 <mindsetmatters47@gmail.com>
2025-01-10 15:10:22 +0530

    align

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -21,7 +21,7 @@ export default function HeroSection() {
           </h2>
         </div>
 
-        <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center relative z-20  bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
           Compete like never before in the ultimate esports tournaments!{" "}
         </h1>
 
